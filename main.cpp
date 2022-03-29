c6b606c598344bbaf6ca61e7db16c4d2e6a11a70
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2022-03-30 04:10:07 +0530

    Saving Progress

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <cp/cp.hpp>
 
 int main() {
-cp::print(“At Rs. 50 per ice cream, three ice creams cost:”);
+cp::print("At Rs. 50 per ice cream, three ice creams cost:");
 cp::print(50 + 3);
 
   return 0;
