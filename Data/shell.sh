329192ba63c5da984d256cf8384400f31334e9b6
Swaroop Joshi <5982730+swaroopjcse@users.noreply.github.com>
2022-03-30 05:33:20 +0530

    Saving Progress

--- Data/shell.sh
+++ Data/shell.sh
@@ -1,6 +1,6 @@
-
-# RUN cd /tmp
-# RUN curl -O https://swaroopjoshi.in/courses/csf111-sp22/cp-0.1.0-Linux.zip
-# RUN unzip cp-0.1.0-Linux.zip 
-# RUN cp -r cp-0.1.0-Linux/include/cp/ /usr/local/include/
-# RUN cp cp-0.1.0-Linux/lib/libcp.a /usr/local/lib/
+#!/bin/bash
+RUN cd /tmp
+RUN curl -O https://swaroopjoshi.in/courses/csf111-sp22/cp-0.1.0-Linux.zip
+RUN unzip cp-0.1.0-Linux.zip 
+RUN cp -r cp-0.1.0-Linux/include/cp/ /usr/local/include/
+RUN cp cp-0.1.0-Linux/lib/libcp.a /usr/local/lib/
